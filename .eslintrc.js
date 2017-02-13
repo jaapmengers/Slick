2621152463d81180d42297c8aff3a4f1eae0c225
Jaap Mengers <jaapm@q42.nl>
2017-02-13 21:30:38 +0100

    Sets up basic server with meteor/apollo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,17 @@
 module.exports = {
-    "extends": "airbnb",
-    "plugins": [
-        "react",
-        "jsx-a11y",
-        "import"
-    ]
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "allowImportExportEverywhere": true
+  },
+  "plugins": [
+    "meteor"
+  ],
+  "extends": [
+    "airbnb",
+    "plugin:meteor/recommended"
+  ],
+  "settings": {
+    "import/resolver": "meteor"
+  },
+  "rules": {}
 };
